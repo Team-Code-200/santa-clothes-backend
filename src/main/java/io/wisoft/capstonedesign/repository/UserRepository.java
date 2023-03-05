d0d80f0a63804fa0c1c6999f23e457b45e8f1776
Jinwon-Dev <yjw9424@naver.com>
2023-03-06 00:44:19 +0900

    feat: 회원 리포지토리 및 서비스 개발

---
+++ src/main/java/io/wisoft/capstonedesign/repository/UserRepository.java
@@ -0,0 +1,34 @@
+package io.wisoft.capstonedesign.repository;
+
+import io.wisoft.capstonedesign.domain.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public User findOne(Long id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+
+    public List<User> findByNickname(String nickname) {
+        return em.createQuery("select u from User u where u.nickname = :nickname", User.class)
+                .setParameter("nickname", nickname)
+                .getResultList();
+    }
+}
