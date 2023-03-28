33c618ce1f5a8870fa26e67b0a2a65169acfe3ac
Jinwon-Dev <yjw9424@naver.com>
2023-03-28 16:18:57 +0900

    feat: 삭제 DTO 개발

---
+++ src/main/java/io/wisoft/capstonedesign/domain/information/web/dto/DeleteInformationResponse.java
@@ -0,0 +1,11 @@
+package io.wisoft.capstonedesign.domain.information.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeleteInformationResponse {
+
+    private Long id;
+}
