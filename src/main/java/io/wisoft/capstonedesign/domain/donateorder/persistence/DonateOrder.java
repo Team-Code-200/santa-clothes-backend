bdec2dc1d75409ae50c31bbfdc5d2b500286fa83
Jinwon-Dev <yjw9424@naver.com>
2023-04-17 02:50:30 +0900

    feat: Builder 패턴 적용

--- src/main/java/io/wisoft/capstonedesign/domain/donateorder/persistence/DonateOrder.java
+++ src/main/java/io/wisoft/capstonedesign/domain/donateorder/persistence/DonateOrder.java
@@ -4,6 +4,7 @@ import io.wisoft.capstonedesign.domain.information.persistence.Information;
 import io.wisoft.capstonedesign.domain.user.persistence.User;
 import io.wisoft.capstonedesign.domain.donate.persistence.Donate;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -44,6 +45,7 @@ public class DonateOrder {
     /**
      * 정적 생성자 메소드
      */
+    @Builder
     public static DonateOrder createDonateOrder(
             final String text,
             final Information information,
