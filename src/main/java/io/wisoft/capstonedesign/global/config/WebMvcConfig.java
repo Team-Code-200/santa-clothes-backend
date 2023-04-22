ee23533fa5d5ef3035b2afa5ed07d7bad769736a
Jinwon-Dev <yjw9424@naver.com>
2023-04-22 15:30:26 +0900

    feat: 로그인 권한 등록을 위한 인터셉터 개발 및 적용

---
+++ src/main/java/io/wisoft/capstonedesign/global/config/WebMvcConfig.java
@@ -0,0 +1,49 @@
+package io.wisoft.capstonedesign.global.config;
+
+import io.wisoft.capstonedesign.global.interceptor.BearerAuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final BearerAuthInterceptor bearerAuthInterceptor;
+
+    public WebMvcConfig(BearerAuthInterceptor bearerAuthInterceptor) {
+        this.bearerAuthInterceptor = bearerAuthInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(bearerAuthInterceptor)
+                .addPathPatterns("/api/users/**")
+
+                .addPathPatterns("/api/donates/new")
+                .addPathPatterns("/api/donates/{id}")
+                .excludePathPatterns("/api/donates/top")
+                .excludePathPatterns("/api/donates/pants")
+                .excludePathPatterns("/api/donates/shoes")
+                .excludePathPatterns("/api/donates/etc")
+
+                .addPathPatterns("/api/finds/new")
+                .addPathPatterns("/api/find/{id}")
+                .excludePathPatterns("/api/finds/top")
+                .excludePathPatterns("/api/finds/pants")
+                .excludePathPatterns("/api/finds/shoes")
+                .excludePathPatterns("/api/finds/etc")
+
+                .addPathPatterns("/api/informations/**")
+
+                .addPathPatterns("/api/shops/**")
+                .excludePathPatterns("/api/shops/details/{id}")
+                .excludePathPatterns("/api/shops")
+
+                .addPathPatterns("/api/donate-orders/**")
+
+                .addPathPatterns("/api/find-orders/**")
+
+                .addPathPatterns("/api/shop-orders/**");
+    }
+}
