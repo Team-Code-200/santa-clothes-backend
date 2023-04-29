cb7ecf213fd047309ff56a947564427aa27b6127
Jinwon-Dev <yjw9424@naver.com>
2023-04-30 00:59:00 +0900

    feat: Swagger 적용을 통한 API 명세 관리 자동화

--- src/main/java/io/wisoft/capstonedesign/domain/chatgpt/web/ChatGptController.java
+++ src/main/java/io/wisoft/capstonedesign/domain/chatgpt/web/ChatGptController.java
@@ -1,5 +1,11 @@
 package io.wisoft.capstonedesign.domain.chatgpt.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.wisoft.capstonedesign.domain.chatgpt.application.ChatGptService;
 import io.wisoft.capstonedesign.domain.chatgpt.web.dto.ChatGptRequest;
 import io.wisoft.capstonedesign.domain.chatgpt.web.dto.ChatGptResponse;
@@ -9,13 +15,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "ChatGPT", description = "ChatGPT 검색 API")
 @RestController
 @RequiredArgsConstructor
 public class ChatGptController {
 
     private final ChatGptService chatGptService;
 
-    @GetMapping("/gptAPI/make/conversation")
+    @Operation(summary = "ChatGPT 검색 사용")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = ChatGptResponse.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(implementation = Error.class)))
+    })
+    @GetMapping("/api/make/conversation")
     public ChatGptResponse makeConversation(@RequestBody @Valid ChatGptRequest chatGptRequest) {
 
         ChatGptResponse chatGptResponse = chatGptService.getConversation(chatGptRequest);
--- src/main/java/io/wisoft/capstonedesign/domain/user/web/UserController.java
+++ src/main/java/io/wisoft/capstonedesign/domain/user/web/UserController.java
@@ -1,5 +1,11 @@
 package io.wisoft.capstonedesign.domain.user.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.wisoft.capstonedesign.domain.user.persistence.User;
 import io.wisoft.capstonedesign.domain.user.web.dto.*;
 import io.wisoft.capstonedesign.domain.user.application.UserService;
@@ -10,12 +16,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Tag(name = "회원", description = "회원 관리 API")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원 가입")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = CreateUserResponse.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+            @ApiResponse(
+                    responseCode = "409",
+                    content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PostMapping("/api/auth/signup")
     public CreateUserResponse saveUser(@RequestBody @Valid final CreateUserRequest request) {
 
@@ -23,6 +42,18 @@ public class UserController {
         return new CreateUserResponse(id);
     }
 
+    @Operation(summary = "회원 정보 수정")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = UpdateUserResponse.class))),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @PatchMapping("/api/users/{id}")
     public UpdateUserResponse updateUser(
             @PathVariable("id") final Long id,
@@ -33,6 +64,18 @@ public class UserController {
         return new UpdateUserResponse(findUser);
     }
 
+    @Operation(summary = "전체 회원 조회")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = Result.class))),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping("/api/users")
     public Result FindUsers() {
         List<User> findUsers = userService.findUsers();
@@ -44,6 +87,18 @@ public class UserController {
         return new Result(collect);
     }
 
+    @Operation(summary = "회원정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = GetUserResponse.class))),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @GetMapping("/api/users/{id}")
     public GetUserResponse getUser(@PathVariable("id") final Long id) {
 
@@ -51,6 +106,18 @@ public class UserController {
         return new GetUserResponse(findUser);
     }
 
+    @Operation(summary = "회원 탈퇴")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = DeleteUserResponse.class))),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(schema = @Schema(implementation = Error.class)))
+    })
     @DeleteMapping("/api/users/{id}")
     public DeleteUserResponse deleteUser(@PathVariable("id") final Long id) {
 
