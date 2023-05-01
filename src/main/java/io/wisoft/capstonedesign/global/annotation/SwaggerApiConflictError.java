2dc7da1c388b7e18acc069dd257483ea17a65a3f
Jinwon-Dev <yjw9424@naver.com>
2023-05-01 15:23:47 +0900

    feat: Swagger 리팩토링을 위한 커스텀 어노테이션 개발

---
+++ src/main/java/io/wisoft/capstonedesign/global/annotation/SwaggerApiConflictError.java
@@ -0,0 +1,26 @@
+package io.wisoft.capstonedesign.global.annotation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Operation()
+@ApiResponses(value = {
+        @ApiResponse(
+                responseCode = "400",
+                content = @Content(schema = @Schema(implementation = Error.class))),
+        @ApiResponse(
+                responseCode = "409",
+                content = @Content(schema = @Schema(implementation = Error.class)))
+})
+public @interface SwaggerApiConflictError {
+}
