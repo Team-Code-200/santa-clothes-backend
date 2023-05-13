d71153b11c5f34ce1c4f6683aa6794cc91edee36
Jinwon-Dev <yjw9424@naver.com>
2023-05-13 18:48:19 +0900

    feat: Redis를 사용하기 위한 설정 파일

---
+++ src/main/java/io/wisoft/capstonedesign/global/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package io.wisoft.capstonedesign.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+public class RedisConfig {
+
+    private final String host;
+    private final int port;
+
+    public RedisConfig(
+            @Value("${spring.data.redis.host}") final String host,
+            @Value("${spring.data.redis.port}") final int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
