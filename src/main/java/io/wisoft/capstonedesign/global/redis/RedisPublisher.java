6d303afbb2f6e510fd6c0c298af0ec6552e52917
Jinwon-Dev <yjw9424@naver.com>
2023-06-15 23:54:42 +0900

    feat: Redis 발행/구독 모델 설정 및 구현

---
+++ src/main/java/io/wisoft/capstonedesign/global/redis/RedisPublisher.java
@@ -0,0 +1,19 @@
+package io.wisoft.capstonedesign.global.redis;
+
+import io.wisoft.capstonedesign.domain.chat.persistence.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisPublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
+
